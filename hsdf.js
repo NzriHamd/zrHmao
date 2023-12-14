f9ae29fed9f9eac266dd7b63dff3f8300ac15de5
NzriHamd <138642371+NzriHamd@users.noreply.github.com>
2023-12-14 15:30:49 +0530

    Create hsdf.js

---
+++ hsdf.js
@@ -0,0 +1,36 @@
+const myelement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+);
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(myelement);
+
+
+const myele3ment = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+);
+
+const conta3iner = document.getElementById('root');
+const r3oot = ReactDOM.createRoot(container);
+root.render(myelement);
