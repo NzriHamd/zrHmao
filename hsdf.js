f1f239ccdc2758e174f4bb1963f2e6ff95444eca
NzriHamd <138642371+NzriHamd@users.noreply.github.com>
2024-01-03 02:14:24 +0530

    Update hsdf.js

--- hsdf.js
+++ hsdf.js
@@ -2,13 +2,17 @@ const myelement = (
   <table>
     <tr>
       <th>Name</th>
+  
     </tr>
+  
     <tr>
       <td>John</td>
     </tr>
+  
     <tr>
       <td>Elsa</td>
     </tr>
+  
   </table>
 );
 
