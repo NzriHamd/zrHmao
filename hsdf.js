7da5a59bcf8b986336797440220ba95354851ebd
NzriHamd <138642371+NzriHamd@users.noreply.github.com>
2024-01-12 20:24:42 +0530

    Update hsdf.js

--- hsdf.js
+++ hsdf.js
@@ -15,7 +15,7 @@ const myelement = (
   </table>
 );
 
-const container = document.getElementById('ew32');
+const container = document.getElementById('ew2');
 const myele3ment = (
   <table>
      <tr>
